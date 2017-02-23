05d6b241a59d1a2622749df0ce130ff6e3270da2
Madessis Sophie <sophie.madessis@student.uclouvain.be>
2017-02-23 22:06:42 +0100

    notif apres 10secondes de timer

--- UCLaConcentration/app/src/main/java/groupe/onze/uclaconcentration/TimerActivity.java
+++ UCLaConcentration/app/src/main/java/groupe/onze/uclaconcentration/TimerActivity.java
@@ -12,6 +12,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import android.app.Activity;
@@ -21,9 +22,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.content.ContentValues.TAG;
+import static android.util.Log.VERBOSE;
 import static groupe.onze.uclaconcentration.R.id.text;
 import static groupe.onze.uclaconcentration.R.id.title;
 
+/* LE TEMPS ECOULE DEPUIS QUE LE TIMER A ETE LANCE EST RECUPERABLE PAR:
+*  mPrefs = getSharedPreferences("label", 0);
+   int seconds = mPrefs.getInt("counterSeconds", "0");
+   int minutes = mPrefs.getInt("counterMinutes", "0");
+* */
 public class TimerActivity extends Activity {
 
     TextView timerTextView;
@@ -35,6 +43,7 @@ public class TimerActivity extends Activity {
     Context context;
     //runs without a timer by reposting this handler at the end of the runnable
     Handler timerHandler = new Handler();
+
     Runnable timerRunnable = new Runnable() {
 
         @Override
@@ -43,7 +52,13 @@ public class TimerActivity extends Activity {
             seconds = (int) (millis / 1000);
             minutes = seconds / 60;
             seconds = seconds % 60;
-            //if(minutes>1) sendNotif();
+            if(seconds>10) {//Envoie une notif après 10 secondes d'écoulées
+                context=getApplicationContext();
+                NotificationsSys.sendNotif(context,"TIME OUT","plus de 10sec",TimerActivity.class);
+            }
+            SharedPreferences.Editor mEditor = mPrefs.edit();
+            mEditor.putInt("CounterSeconds",seconds);
+            mEditor.putInt("CounterMinutes",minutes);
             timerTextView.setText(String.format("%d:%02d", minutes, seconds));
             timerHandler.postDelayed(this, 500);
         }
@@ -55,10 +70,13 @@ public class TimerActivity extends Activity {
         setContentView(R.layout.activity_timer);
 
         timerTextView = (TextView) findViewById(R.id.textViewtime);
+        /* Affiche le temps actuel écoulé*/
         mPrefs = getSharedPreferences("label", 0);
-        String mString = mPrefs.getString("timeTotal", "0");
+        int seconds = mPrefs.getInt("seconds", 0);//0=def value
+        int minutes = mPrefs.getInt("minutes", 0);//0=def value
+        String count = String.format("%d:%02d", minutes, seconds);
         textView2 = (TextView) findViewById(R.id.timetotal);
-        textView2.setText(mString);
+        textView2.setText(count);
 
         Button b = (Button) findViewById(R.id.buttonTimer);
         b.setText("start");
@@ -67,12 +85,9 @@ public class TimerActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Button b = (Button) v;
-                context=getApplicationContext();
-                NotificationsSys.sendNotif(context,"test titre","test descr",TimerActivity.class);
+
                 if (b.getText().equals("stop")) {
                     timerHandler.removeCallbacks(timerRunnable);
-                    SharedPreferences.Editor mEditor = mPrefs.edit();
-                    mEditor.putString("timeTotal", String.format("%d:%02d", minutes, seconds)).commit();
                     b.setText("start");
                 } else {
                     startTime = System.currentTimeMillis();
@@ -94,10 +109,37 @@ public class TimerActivity extends Activity {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        Log.v(TAG, "++ ON START ++");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v(TAG, "+ ON RESUME +");
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.v(TAG, "-- ON STOP --");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "- ON DESTROY -");
+    }
+
+    @Override
     public void onPause() {
         super.onPause();
+        Log.v(TAG, "- ON PAUSE -");
         timerHandler.removeCallbacks(timerRunnable);
         Button b = (Button)findViewById(R.id.buttonTimer);
         b.setText("start");
     }
+
 }
