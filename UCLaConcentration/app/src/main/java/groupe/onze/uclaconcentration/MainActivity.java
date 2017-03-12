96a84f72e8fdb6b824e5a767beca2ca94af4868e
Madessis Sophie <sophie.madessis@student.uclouvain.be>
2017-03-12 17:19:04 +0100

    icone menu

--- UCLaConcentration/app/src/main/java/groupe/onze/uclaconcentration/MainActivity.java
+++ UCLaConcentration/app/src/main/java/groupe/onze/uclaconcentration/MainActivity.java
@@ -7,10 +7,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends BasicActivity {
     TimerServiceReceiver timerReceiver;
@@ -22,6 +26,9 @@ public class MainActivity extends BasicActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=(TextView) findViewById(R.id.tv1);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         //Acces au store
         Button store = (Button) findViewById(R.id.button_store);
@@ -94,6 +101,29 @@ public class MainActivity extends BasicActivity {
         }
         Log.i("isMyServiceRunning?", false + "");
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_favorite) {
+            Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
