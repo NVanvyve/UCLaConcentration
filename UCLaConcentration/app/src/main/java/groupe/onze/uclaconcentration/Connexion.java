a1f9818894ec38918b638cc71417684bf1d57dcc
Vanvyve Nicolas <nicolas.vanvyve@student.uclouvain.be>
2017-03-12 23:06:50 +0100

    Oops

--- UCLaConcentration/app/src/main/java/groupe/onze/uclaconcentration/Connexion.java
+++ UCLaConcentration/app/src/main/java/groupe/onze/uclaconcentration/Connexion.java
@@ -1,7 +1,7 @@
 package groupe.onze.uclaconcentration;
 
 import android.content.Intent;
--import android.os.Bundle;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
